bfc29dd4930f99eacb299b80a5f152e186e24987
sawanoboly <sawanoboriyu@higanworks.com>
2016-01-17 18:01:04 +0900

    support debian family

--- lib/itamae/plugin/recipe/nginx_build.rb
+++ lib/itamae/plugin/recipe/nginx_build.rb
@@ -9,8 +9,19 @@ nginx_build_version = node[:nginx_build][:version] if node[:nginx_build] && node
 nginx_build_bin = "/usr/local/bin/"
 nginx_build_bin = node[:nginx_build][:bin] if node[:nginx_build] && node[:nginx_build][:bin]
 
-%w(pcre pcre-devel).each do |pkg|
-  package pkg
+case node[:platform]
+when 'debian', 'ubuntu'
+  execute "apt-get update" do
+    command "apt-get update"
+  end
+
+  %w(build-essential libpcre3 libpcre3-dev libssl-dev).each do |pkg|
+    package pkg
+  end
+else
+  %w(pcre pcre-devel).each do |pkg|
+    package pkg
+  end
 end
 
 execute "nginx-build install" do
@@ -24,3 +35,4 @@ EOS
   command nginx_build
   not_if "test -e #{nginx_build_bin}nginx-build"
 end
+
